3ba24bd3ff8fc48aff0e9b6182662db242cfe9f9
campjake <campbell.jakelog@gmail.com>
2023-03-16 16:41:09 -0700

    Cleaned up the driver for toUpperCase, and everything checks out okay.

--- drivers/toUpperDriver.s
+++ drivers/toUpperDriver.s
@@ -5,7 +5,12 @@ sz1:	.asciz	"aaaa"
 	.text
 
 _start:
-	LDR	X0,=sz1
-	BL	toUpperCase
-	LDR	X0, [X0]		// Just added for break
-	MOV	X1, #0			// Does nothing
+	LDR	X0,=sz1		// Load string
+	BL	toUpperCase	// X0 points to sz1 all upper-case
+	LDR	X2,=sz1		// Break here if needed
+	LDR	X2, [X2]	// Check if its upper-case
+
+// Setup prarameters to exit program and call Linux kernel to do so
+        MOV     X0, #0		// Use return code 0
+	MOV	X8, #93		// Service command 93
+	SVC	0		// Bye-bye :)
