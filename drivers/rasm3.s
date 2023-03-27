e769f524e02694c1c1d853917bc5297a6af5ebd1
gshane00 <greg.shane00@gmail.com>
2023-03-27 11:18:43 -0700

    updates added to rasm3

--- drivers/rasm3.s
+++ drivers/rasm3.s
@@ -17,6 +17,7 @@ szInput1:	.skip	21	// input for string1
 szInput2:	.skip	21	// input for string2
 szInput3:	.skip	21	// input for string3
 szOutput:	.skip	21	// output string
+szIndex:	.skip	21	// index value
 
 str4ptr:	.quad	0	// string4 pointer
 szSubstring:	.quad	0	// pointer for substring
@@ -24,50 +25,49 @@ bChar:		.byte	0	// char (for results of charAt subroutine)
 concatPtr1:	.quad	0	// ptr for concat1
 concatPtr2:	.quad	0	// ptr for concat2
 
+szLength1:	.asciz	"1. S1.length() = "
+szLength2:	.asciz	"   S2.length() = "
+szLength3:	.asciz	"   S3.length() = "
 
-szLength1:	.asciz	"S1.length() = "
-szLength2:	.asciz	"S2.length() = "
-szLength3:	.asciz	"S3.length() = "
+szEqual1:	.asciz	"2. String_equals(s1,s3) = "
+szEqual2:	.asciz	"3. String_equals(s1,s1) = "
+szEqIC1:	.asciz	"4. String_equalsIgnoreCase(s1,s3) = "
+szEqIC2:	.asciz	"5. String_equalsIgnoreCase(s1,s2) = "
 
-szEqual1:	.asciz	"String_equals(s1,s3) = "
-szEqual2:	.asciz	"String_equals(s1,s1) = "
-szEqIC1:	.asciz	"String_equalsIgnoreCase(s1,s3) = "
-szEqIC2:	.asciz	"String_equalsIgnoreCase(s1,s2) = "
+szStrCopy:	.asciz	"6. s4 = String_copy(s1)\n"
+szStr1:		.asciz	"   s1 = "
+szStr4:		.asciz	"   s4 = "
 
-szStrCopy:	.asciz	"s4 = String_copy(s1)\n"
-szSub1:		.asciz	"String_substring_1(s3,4,14) = "
-szSub2:		.asciz	"String_substring_2(s3,7) = "
+szSub1:		.asciz	"7. String_substring_1(s3,4,14) = "
+szSub2:		.asciz	"8. String_substring_2(s3,7) = "
 
-szStr1:		.asciz	"s1 = "
-szStr4:		.asciz	"s4 = "
 
-szCharAt:	.asciz	"String_charAt(s2,4) = "
+szCharAt:	.asciz	"9. String_charAt(s2,4) = "
 
-szStartsWith1:	.asciz	"String_startswWith_1(s1, 11, \"hat.\") = "
+szStartsWith1:	.asciz	"10. String_startswWith_1(s1, 11, \"hat.\") = "
 szStarts1:	.asciz	"hat."
-szStartsWith2:	.asciz	"String_startsWith_2(s1, \"Cat\") = "
+szStartsWith2:	.asciz	"11. String_startsWith_2(s1, \"Cat\") = "
 szStarts2:	.asciz	"Cat"
 
-szEndsWith:	.asciz	"String_endsWith(s1, \"in the hat.\") = "
+szEndsWith:	.asciz	"12. String_endsWith(s1, \"in the hat.\") = "
 szEnds:		.asciz	"in the hat."
 
-szIndexOf1:	.asciz	"String_indexOf_1(s2, 'g') = "
-szIndexOf2:	.asciz	"String_indexOf_2(s2, 'g', 9) = "
-szIndexOf3:	.asciz	"String_indexOf_3(s2, \"eggs\") = "
+szIndexOf1:	.asciz	"13. String_indexOf_1(s2, 'g') = "
+szIndexOf2:	.asciz	"14. String_indexOf_2(s2, 'g', 9) = "
+szIndexOf3:	.asciz	"15. String_indexOf_3(s2, \"eggs\") = "
 
-szLastIndexOf1:	.asciz	"String_lastIndexOf_1(s2, 'g') = "
-szLastIndexOf2:	.asciz	"String_lastIndexOf_2(s2, 'g', 6) = "
-szLastIndexOf3:	.asciz	"String_lastIndexOf_3(s2, \"egg\") = "
+szLastIndexOf1:	.asciz	"16. String_lastIndexOf_1(s2, 'g') = "
+szLastIndexOf2:	.asciz	"17. String_lastIndexOf_2(s2, 'g', 6) = "
+szLastIndexOf3:	.asciz	"18. String_lastIndexOf_3(s2, \"egg\") = "
 szIndexOfSub1:	.asciz	"eggs"
 szIndexOfSub2:	.asciz	"egg"
-szIndex:	.skip	21
 
-szReplace:	.asciz	"String_replace(s1, 'a', 'o') = "
-szLower:	.asciz	"String_toLowerCase(s1) = "
-szUpper:	.asciz	"String_toUpperCase(s1) = "
+szReplace:	.asciz	"19. String_replace(s1, 'a', 'o') = "
+szLower:	.asciz	"20. String_toLowerCase(s1) = "
+szUpper:	.asciz	"21. String_toUpperCase(s1) = "
 
-szConcat1:	.asciz	"String_concat1(s1, \" \");\n"
-szConcat2:	.asciz	"String_concat2(s1, s2) = "
+szConcat1:	.asciz	"22. String_concat1(s1, \" \");\n"
+szConcat2:	.asciz	"    String_concat2(s1, s2) = "
 szSpace:	.asciz	" "
 
 szHeadingNames: .asciz	"Team 14: Gregory Shane & Jacob Campbell"
@@ -85,33 +85,32 @@ chCr:		.byte	10	// ascii carriage return
 // Print heading
 _start:
 	LDR	X0,=szHeadingNames	// *X0 = names
-	BL	putstring			// Print names
+	BL	putstring		// Print names
 
-	LDR	X0,=chCr			// Load line feed
-	BL	putch				// Print line feed
+	LDR	X0,=chCr		// Load line feed
+	BL	putch			// Print line feed
 
 	LDR	X0,=szHeadingClass	// *X0 = class
-	BL	putstring			// Print class
+	BL	putstring		// Print class
 
-	LDR	X0,=chCr			// Load line feed
-	BL	putch				// Print line feed
+	LDR	X0,=chCr		// Load line feed
+	BL	putch			// Print line feed
 
 	LDR	X0,=szHeadingAsgn	// *X0 = assignment
-	BL	putstring			// Print assignment
+	BL	putstring		// Print assignment
 
-	LDR	X0,=chCr			// Load line feed
-	BL	putch				// Print line feed
+	LDR	X0,=chCr		// Load line feed
+	BL	putch			// Print line feed
 
 	LDR	X0,=szHeadingDate	// *X0 = date
-	BL	putstring			// Print date
+	BL	putstring		// Print date
 
-	LDR	X0,=chCr			// Load line feed
-	BL	putch				// Print line feed
+	LDR	X0,=chCr		// Load line feed
+	BL	putch			// Print line feed
+	LDR	X0,=chCr		// Load line feed
+	BL	putch			// Print line feed
 
-	LDR	X0,=chCr			// Load line feed
-	BL	putch				// Print line feed
-
-// Prompting inputs	
+// Prompting inputs
 	LDR x0, =szPrompt	// points to szPrompt
 	BL  putstring		// displays to terminal
 
@@ -133,6 +132,9 @@ _start:
 	MOV x1,#SIZE		// max input size is 21 characters
 	BL  getstring		// CIN >> string3
 
+	LDR x0,=chCr		// points to carriage return
+	BL  putch		// display to terminal
+
 // Converting string_length and displaying
 	LDR x0,=szInput1	// points to szInput1
 	BL  String_length	// string1.length()
@@ -145,8 +147,6 @@ _start:
 	BL  putstring		// displays to terminal
 	LDR x0,=chCr		// points to carriage return
 	BL  putch		// displays to terminal
-	LDR x0, =chCr		// points to carriage return
-	BL  putch		// displays to terminal
 
 	LDR x0,=szInput2	// points to szInput2
 	BL  String_length	// string2.length()
@@ -157,8 +157,6 @@ _start:
 	BL  putstring		// displays to terminal
 	LDR x0,=szOutput	// points to szOutput
 	BL  putstring		// displays to terminal
-	LDR x0,=chCr		// points to carriage return
-	BL  putch		// displays to terminal
 	LDR x0,=chCr		// points to carriage return
 	BL  putch		// displays to terminal
 
